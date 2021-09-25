87d750befcb64818d47407ff7c167c115011e4f1
Brunix Michel <brunixmichel@Brunixs-MacBook-Air.local>
2021-09-25 15:42:40 -0400

    Added styling and logo to the Navbar

--- shellhacks_submission/src/pages/MapView.js
+++ shellhacks_submission/src/pages/MapView.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Button } from "react-bootstrap"
-
+import { Dashboard } from "../components/Dashboard";
 
 function MapView(){
     return(
+    <div>
     <Button>MapView</Button>
+    <Dashboard></Dashboard>
+    </div>
     )
 
 
