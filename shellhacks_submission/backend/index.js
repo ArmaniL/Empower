9965db12af216dc2bca7a0b89c6ccb1cd08a84af
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2021-09-25 16:57:25 -0400

    add id route to the backend

--- shellhacks_submission/backend/index.js
+++ shellhacks_submission/backend/index.js
@@ -6,12 +6,33 @@ const port  = process.env.PORT || 8080;
 
 
 app.use(cors())
+app.get('/id',async(req,res)=>{
+
+  let axios = require('axios');
+
+    let config = {
+      method: 'get',
+      url: 'https://api.yelp.com/v3/businesses/'+req.query.id,
+      headers: { 
+        'Authorization': 'Bearer joCHI4iyiFsdWXevs76e3NDMoand-ZtkGOse3e-nPQXFJmsiP8gbwILFJawdYlQzrJgU1HqHyNuA9q2HqAkcIhnlpc60y1jRlkF3imHgNU4DTlgM4au8YGaP7wsiX3Yx'
+      }
+    };
+    
+    axios(config)
+    .then(function (response) {
+    res.send(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+})
 app.get('/', async (req, res) => {
     var axios = require('axios');
 
     var config = {
       method: 'get',
-      url: 'https://api.yelp.com/v3/businesses/search?term="hispanic"&location="'+req.query.location+"\"",
+      url: 'https://api.yelp.com/v3/businesses/search?term="small"&location="'+req.query.location+"\"",
       headers: { 
         'Authorization': 'Bearer joCHI4iyiFsdWXevs76e3NDMoand-ZtkGOse3e-nPQXFJmsiP8gbwILFJawdYlQzrJgU1HqHyNuA9q2HqAkcIhnlpc60y1jRlkF3imHgNU4DTlgM4au8YGaP7wsiX3Yx'
       }
